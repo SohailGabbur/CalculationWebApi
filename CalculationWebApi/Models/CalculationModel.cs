87de68690112d0c7031b2cd8ccb4fc8d42106103
SohailGabbur <sohail07gabbur@gmail.com>
2020-09-21 15:35:13 +0530

    Add project files.

---
+++ CalculationWebApi/Models/CalculationModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CalculationWebApi.Models
+{
+    public class CalculationModel
+    {
+        [Required(ErrorMessage ="Please enter the number")]
+        public int Num1 { get; set; }
+        [Required(ErrorMessage ="Please enter the number")]
+        public int Num2 { get; set; }
+    }
+}
